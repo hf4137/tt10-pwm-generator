a9d3d3d555074190ec7aed21ddaeb893ca8ef748
hf4137 <hf4137@wayne.edu>
2025-12-05 22:13:35 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -3,15 +3,15 @@
 
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+from cocotb.triggers import ClockCycles, Timer
 
 
 @cocotb.test()
 async def test_project(dut):
     dut._log.info("Start")
 
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, units="us")
+    # Set the clock period to 10 ns (100 MHz)
+    clock = Clock(dut.clk, 10, units="ns")
     cocotb.start_soon(clock.start())
 
     # Reset
@@ -25,16 +25,39 @@ async def test_project(dut):
 
     dut._log.info("Test project behavior")
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 5) 
+    dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 5)
 
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+    #
+    # Increase duty cycle 10 times
+    #
+    
+    dut._log.info("Increasing duty cycle…")
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    for _ in range(10):
+        dut.ui_in[0].value = 1      # increase duty
+        await Timer(25, units="ns")
+        dut.ui_in[0].value = 0
+        await Timer(25, units="ns")
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    #
+    # Decrease duty cycle 10 times
+    #
+
+    dut._log.info("Decreasing duty cycle…")
+
+    for _ in range(10):
+        dut.ui_in[1].value = 1      # decrease duty
+        await Timer(25, units="ns")
+        dut.ui_in[1].value = 0
+        await Timer(25, units="ns")
+
+    #
+    # Allow time to observe PWM output
+    #
+    
+    await ClockCycles(dut.clk, 200)
+
+    dut._log.info("PWM generator cocotb test completed")
